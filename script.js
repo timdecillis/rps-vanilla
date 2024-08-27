6dcf7a3ef351eea9c2fdf21a5e4768196ef6209b
timdecillis <timdecillis@gmail.com>
2024-08-26 22:19:37 -0700

    completed game with button

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 let count = 0;
+const playButton = document.querySelector("#play-round");
+playButton.addEventListener("click", playRound);
 
 function playRound() {
   const winningCombos = {
@@ -27,36 +29,32 @@ function playRound() {
   const computerChoice = getComputerChoice();
   let message = "";
 
+  const humanScoreCount = document.querySelector("#your-score");
+  const computerScoreCount = document.querySelector("#computer-score");
+  const roundCount = document.querySelector("#round-count");
+
   if (humanChoice === computerChoice) {
     message = "draw";
   } else if (computerChoice === winningCombos[humanChoice]) {
     humanScore++;
+    humanScoreCount.textContent = `Your score: ${humanScore}`;
     message = `${humanChoice} beats ${computerChoice}, you win! the score is ${humanScore}-${computerScore}`;
   } else {
     computerScore++;
+    computerScoreCount.textContent = `Computer score: ${computerScore}`;
     message = `${computerChoice} beats ${humanChoice}, you lose! the score is ${humanScore}-${computerScore}`;
   }
   count++;
-  return window.alert(message);
-}
-
-const playGame = () => {
-  // while (count < 5) {
-  //   playRound();
-  // }
-  const playButton = document.querySelector("#play-round");
-  playButton.addEventListener("click", playRound);
+  roundCount.textContent = `Round number: ${count}`;
+  window.alert(message);
   if (count === 5) {
+    playButton.disabled = true;
     if (humanScore > computerScore) {
       return window.alert(`you won the game, ${humanScore}-${computerScore}!`);
     }
     if (computerScore > humanScore) {
       return window.alert(`you lost the game, ${humanScore}-${computerScore}!`);
     }
+    return window.alert("the game is a draw");
   }
-  // const yourScore = document.querySelector(".your-score");
-  // yourScore.textContent = "FOO";
-  // return window.alert(`the game is a draw, ${humanScore}-${computerScore}`);
-};
-
-playGame();
+}
